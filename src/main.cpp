b7b004d57a48a515968981a39bc25935cd846435
bwegrzyn0 <bwegrzyn@proton.me>
2026-06-18 18:18:55 +0200

    The user can rotate the camera around the origin, where the mesh is placed

--- src/main.cpp
+++ src/main.cpp
@@ -54,41 +54,40 @@ float vertices[] = {
 };
 
 
-// zmiana rozmiaru okna
+// adjust window size
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);	
 }
 
-glm::vec3 cameraPos = glm::vec3(0.0f);
-glm::vec3 cameraV = glm::vec3(0.0f);
-float cameraVValue = 5.0f;
+// camera 1. polar angle and 2. asimuth
+glm::vec2 cameraAngles = glm::vec2(0.0f, 0.0f);
+// velocity of the camera
+glm::vec2 cameraV = glm::vec2(0.0f);
+// distance of the camera from the origin 
+float cameraDistance = 3.0f;
+// speed of camera rotation
+float cameraVValue = 2.0f;
 
-// wejście
+// input
 void processInput(GLFWwindow* window) {
 	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 	if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		cameraV.z = -cameraVValue; 
-	else if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		cameraV.z = cameraVValue; 
-	else 
-		cameraV.z = 0.0f;
-	if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		cameraV.x = -cameraVValue; 
-	else if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		cameraV.x = cameraVValue; 
-	else
+	else if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraV.x = -cameraVValue; 
+	else 
 		cameraV.x = 0.0f;
-	if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-		cameraV.y = -cameraVValue; 
-	else if(glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+	if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
 		cameraV.y = cameraVValue; 
+	else if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraV.y = -cameraVValue; 
 	else
 		cameraV.y = 0.0f;
 }
 
 int main() {
-	// okno
+	// window 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -112,7 +111,7 @@ int main() {
 
 	glEnable(GL_DEPTH_TEST);
 	
-	// shader program for normal cubes
+	// shader program for meshes
 	Shader shaderProgram("src/vertexShader.vert", "src/fragmentShader.frag");
 	shaderProgram.use();
 
@@ -120,17 +119,12 @@ int main() {
 	proj = glm::perspective(45.0f, (float(WIDTH))/((float)HEIGHT), 0.1f, 100.0f);
 	shaderProgram.setMat4("proj", proj);
 
-	Mesh cube(vertices, sizeof(vertices), NULL, shaderProgram, glm::vec3(0.0f, 0.0f, -3.0f),  glm::vec3(1.0f, 0.0f, 0.0f));
-	cube.setAngularMomentum(glm::vec3(0.0f, 5.0f, 0.001f));
-//	cube.rotationMatrix = glm::rotate(cube.rotationMatrix, 0.2f, glm::vec3(1.0f, 0.0f, 0.0f));
+	Mesh cube(vertices, sizeof(vertices), NULL, shaderProgram, glm::vec3(0.0f, 0.0f, 0.0f),  glm::vec3(1.0f, 0.0f, 0.0f));
+	cube.setAngularMomentum(glm::vec3(0.0f, 0.0f, 0.0f));
+	// cube.rotationMatrix = glm::rotate(cube.rotationMatrix, 0.2f, glm::vec3(1.0f, 0.0f, 0.0f));
 	cube.MoI = glm::vec3(1.0f, 3.0f, 6.0f);
 
-	// shader program for lights
-	//Shader lightProgram("src/vertexShader.vert", "src/lightFragmentShader.frag");
-	//lightProgram.setMat4("proj", proj);
-
 	glm::vec3 lightPos = glm::vec3(2.0f, 2.0f, 4.0f);
-	//Mesh light(vertices, sizeof(vertices), NULL, lightProgram, lightPos, glm::vec3(1.0f));
 	shaderProgram.setVec3("lightPos", lightPos);
 
 	float lastTime = (float) glfwGetTime();
@@ -141,11 +135,12 @@ int main() {
 		float dT = now - lastTime;
 		lastTime = now;
 
-		cameraPos += cameraV * dT;
+		cameraAngles += cameraV * dT;
 		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, -cameraPos);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -cameraDistance));
+		view = glm::rotate(view, cameraAngles.x, glm::vec3(1.0f, 0.0f, 0.0f));
+		view = glm::rotate(view, cameraAngles.y, glm::vec3(0.0f, 1.0f, 0.0f));
 		shaderProgram.setMat4("view", view);
-		//lightProgram.setMat4("view", view);
 
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
--- src/mesh.cpp
+++ src/mesh.cpp
@@ -62,28 +62,29 @@ void Mesh::update(float dT) {
 	if (omega.y == 0 && omega.z >= 0) {
 		omega.y = -A * glm::sin(sqrt_omegas * dT);
 		omega.z = A * k * glm::cos(sqrt_omegas * dT);
-	} else if (omega.y == 0 && omega.z < 0) {
+	} else if (omega.y == 0 && omega.z < 0) { // correction when atan returns pi (numerical error accumulates leading to inaccurate results)
 		omega.y = A * glm::sin(sqrt_omegas * dT);
 		omega.z = -A * k * glm::cos(sqrt_omegas * dT);
 	} else {
 		omega.y = A * glm::cos(sqrt_omegas * dT + phase);
 		omega.z = A * k * glm::sin(sqrt_omegas * dT + phase);
 	}
-	// correction when atan returns pi (numerical error accumulates leading to inaccurate results)
 
+	// nearby values are conserved, those oscillate a bit
 	float E = omega.x * omega.x * MoI[0] + omega.y * omega.y * MoI[1] + omega.z * omega.z * MoI[2];
 	float M = omega.x * omega.x * MoI[0] * MoI[0] + omega.y * omega.y * MoI[1] * MoI[1] + omega.z * omega.z * MoI[2] * MoI[2];
 	std::cout << "Energy: " <<  E << std::endl;
 	std::cout << "Angular momentum: " << M << std::endl;
 
+	// rotate the model appropriately
 	float omegaLength = glm::length(omega);
 	glm::vec3 omega_worldspace = glm::vec3(glm::inverse(rotationMatrix) * glm::vec4(omega, 1.0f));
-
-	rotationMatrix = glm::rotate(rotationMatrix, omegaLength * dT, omega_worldspace);
-
+	if (omegaLength != 0)
+		rotationMatrix = glm::rotate(rotationMatrix, omegaLength * dT, omega_worldspace);
+	// apply all the transformations
 	model = trans * rotationMatrix * model;
 	shaderProgram.setMat4("model", model);
-	// transpose inverse for the normals
+	// transpose inverse for the normals (it's costly for the shader so we do it here)
 	model = glm::inverse(model);
 	model = glm::transpose(model);
 	shaderProgram.setMat4("transposeInverseModel", model);
